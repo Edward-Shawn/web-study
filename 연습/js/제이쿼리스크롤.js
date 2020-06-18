e8052d384bcfb9d61f6e57bd7efbdbcc9cd27395
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-06-18 20:52:24 +0900

    제이쿼리 스크롤 이벤트 연습
    a 태그에   벨류값을 지정하여  클릭시 섹션으로 이동시 상단 끝에 네비게이션바  걸치는 기능 구현
    자바스크립트에서 클릭 move 값으로 이동되는것과는 다른것.

---
+++ 연습/js/제이쿼리스크롤.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    let hedTop = $("header").offset().top;
+    $(window).scroll(function(){
+        let scroll = $(window).scrollTop();
+
+        if(scroll>=hedTop){
+            $("header").addClass("scroll");
+        }else{
+            $("header").removeClass("scroll");
+        }
+    });
+    
+    $(".gnb li").click(function(){
+        let valuePos = $(this).find("a").attr("value");
+        let offsetTop = $("#"+valuePos).offset().top;
+            $("html,body").animate({scrollTop:offsetTop -50}, 500);
+    });
+
+})
